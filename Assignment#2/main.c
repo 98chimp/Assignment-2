2ca93336eb51e15e7b157c970439ab03f41ab568
Shahin Zangenehpour <shahin@98chimp.com>
2016-11-27 19:55:57 -0800

    Initial Commit

---
+++ Assignment#2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Assignment#2
+//
+//  Created by Shahin on 2016-11-27.
+//  Copyright © 2016 98%Chimp. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
